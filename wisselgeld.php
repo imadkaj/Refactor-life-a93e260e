2b1ca191c3a7034732aa5e76b535fd7f7aa720b7
FIRST_Imad LAST_Kajdouh <2058092@talnet.nl>
2019-11-29 15:01:20 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,24 @@
+<?php
+
+$input = ($argv[1]);
+$geldeenheden = array(50, 20, 10, 5, 2, 1);
+$centen = array(50, 20 ,10 ,5);
+$geld = intval($input);
+$cent = $input - $geld;
+$cent = intval(round($cent * 100));
+$cent = round($cent / 5) * 5;
+
+foreach ($geldeenheden as $value) {
+    $restgeld = floor($geld / $value);
+    if ($restgeld >= 1) {
+        $geld = $geld - ($value * $restgeld);
+        echo $restgeld . " x " . $value . " euro" . PHP_EOL;
+    }
+}
+foreach ($centen as $value) {
+    $restcent = floor($cent / $value);
+    if ($restcent >= 1) {
+        $cent = $cent - ($value * $restcent);
+        echo $restcent . " x " . $value . " cent" . PHP_EOL;
+    }
+}
